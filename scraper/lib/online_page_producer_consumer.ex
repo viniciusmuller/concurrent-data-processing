3f664d4be69a1c8eacdc915fc4e25d8eb1ff25ad
Vinícius Müller <vinigm.nho@gmail.com>
2023-01-14 22:54:37 -0300

    refactor: use Flow as a :producer_consumer instead of plain GenStage

--- scraper/lib/online_page_producer_consumer.ex
+++ scraper/lib/online_page_producer_consumer.ex
@@ -4,31 +4,17 @@ defmodule OnlinePageProducerConsumer do
   then sends them to the next step when there's demand
   """
 
-  use GenStage
+  use Flow
 
-  require Logger
+  def start_link(_args) do
+    producers = [Process.whereis(PageProducer)]
 
-  def start_link(id) do
-    GenStage.start_link(__MODULE__, [], name: via(id))
-  end
-
-  def init(initial_state) do
-    Logger.info("OnlinePageProducerConsumer init")
-
-    subscription = [
-      {PageProducer, min_demand: 0, max_demand: 1}
+    consumers = [
+      {Process.whereis(PageConsumerSupervisor), max_demand: 2}
     ]
 
-    {:producer_consumer, initial_state, subscribe_to: subscription}
-  end
-
-  def handle_events(events, _from, state) do
-    Logger.info("OnlinePageProducer received #{inspect(events)}")
-    events = Enum.filter(events, &Scraper.online?/1)
-    {:noreply, events, state}
-  end
-
-  def via(id) do
-    {:via, Registry, {ProducerConsumerRegistry, id}}
+    Flow.from_stages(producers, max_demand: 1, stages: 2)
+    |> Flow.filter(&Scraper.online?/1)
+    |> Flow.into_stages(consumers)
   end
 end
--- scraper/lib/scraper/application.ex
+++ scraper/lib/scraper/application.ex
@@ -11,9 +11,8 @@ defmodule Scraper.Application do
       {Registry, keys: :unique, name: ProducerConsumerRegistry},
       # the same as {PageProducer, []}
       PageProducer,
-      producer_consumer_spec(id: 1),
-      producer_consumer_spec(id: 2),
-      PageConsumerSupervisor
+      PageConsumerSupervisor,
+      OnlinePageProducerConsumer,
       # Supervisor.child_spec(PageConsumer, id: :consumer_a),
       # Supervisor.child_spec(PageConsumer, id: :consumer_b),
       # Supervisor.child_spec(PageConsumer, id: :consumer_c),
@@ -23,10 +22,5 @@ defmodule Scraper.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Scraper.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  defp producer_consumer_spec(id: id) do
-    id = "online_page_producer_consumer_#{id}"
-    Supervisor.child_spec({OnlinePageProducerConsumer, id}, id: id)
   end
 end
